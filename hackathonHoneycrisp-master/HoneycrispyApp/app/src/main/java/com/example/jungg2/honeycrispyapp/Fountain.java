1af1adc543606e85e2a52421e98b0894b3f460c4
Kevin Doan <doanm3@wwu.edu>
2018-02-10 14:37:17 -0800

    Updated Object: Building > Fountain Test File: test Useless Algorithm: ratingAlgo

---
+++ hackathonHoneycrisp-master/HoneycrispyApp/app/src/main/java/com/example/jungg2/honeycrispyapp/Fountain.java
@@ -0,0 +1,19 @@
+/**
+ * Created by doanm3 on 2/10/18.
+ */
+public class Fountain {
+    private int location;
+    //private int rating;
+
+    public Fountain(int id){
+        location = id;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    //rating method call;
+
+
+}
---
+++ hackathonHoneycrisp-master/HoneycrispyApp/app/src/main/java/com/example/jungg2/honeycrispyapp/ratingAlgo.java
@@ -0,0 +1,12 @@
+/**
+ * Created by doanm3 on 2/10/18.
+ */
+public class ratingAlgo {
+    private int positive = 5;
+    private int negative = 5;
+    private double percentage = 0.0;
+
+    public double getPercentage() {
+        return percentage;
+    }
+}
